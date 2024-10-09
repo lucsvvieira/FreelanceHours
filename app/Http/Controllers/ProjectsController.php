d208692e7fe29566b90e11dc942d00878d4cdf33
Lucas Vieira <vieira07lucas@gmail.com>
2024-10-09 15:47:36 -0300

    criando livewire components

---
+++ app/Http/Controllers/ProjectsController.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Project;
+use Illuminate\Http\Request;
+
+class ProjectsController extends Controller
+{
+    public function Index ()
+
+    {
+
+        return view('projects.index');
+        
+    }
+
+    public function show(Project $project)
+
+    {
+
+        return view('projects.show', compact('project'));
+
+    }
+}
