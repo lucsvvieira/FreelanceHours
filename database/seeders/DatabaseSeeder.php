81cf8aad8efafb46f8f3bc374d817dfce9b4eecd
Lucas Vieira <vieira07lucas@gmail.com>
2024-10-13 12:30:52 -0300

    atualizando posicoes

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,11 +2,13 @@
 
 namespace Database\Seeders;
 
+use App\Actions\ArrangePositions;
 use App\Models\Project;
 use App\Models\Proposal;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
 
 class DatabaseSeeder extends Seeder
 {
@@ -15,19 +17,20 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        
+
         User::factory()->count(200)->create();
 
         User::query()->inRandomOrder()->limit(10)->get()
 
-        ->each(function (User $u) {
-            
-            $project = Project::factory()->create(['created_by' => $u->id]);
+            ->each(function (User $u) {
 
-            Proposal::factory()->count(random_int(4,45))->create(['project_id' => $project->id]);
-        
-    });
-        
-        
+                $project = Project::factory()->create(['created_by' => $u->id]);
+
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+
+                ArrangePositions::run($project->id);
+
+
+            });
     }
 }
