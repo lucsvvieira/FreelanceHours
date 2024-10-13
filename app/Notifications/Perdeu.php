2936145f3009c6ff50147a5b6e0bdf8ea140b06d
Lucas Vieira <vieira07lucas@gmail.com>
2024-10-13 13:38:56 -0300

    Finalização do projeto

--- app/Notifications/Perdeu.php
+++ app/Notifications/Perdeu.php
@@ -8,7 +8,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class Perdeu extends Notification
+class Perdeu extends Notification implements ShouldQueue
 {
     use Queueable;
 
